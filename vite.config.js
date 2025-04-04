734817d9a6c6f984258fa5c26be07ffb5b91300a
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-04-03 21:17:06 -0600

    Agregar configuración de proxy en vite.config.js para redirigir solicitudes API al backend local

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    proxy: {
+      '/api': { // Redirigir las solicitudes que empiezan con /api
+        target: 'http://localhost:5173/', // Cambia esto por la URL de tu backend
+        changeOrigin: true,
+        secure: false, // Si tu backend usa HTTPS con un certificado autofirmado
+        rewrite: (path) => path.replace(/^\/api/, '') // Opcional: reescribe la ruta
+      }
+    }
+  }
  
 })
