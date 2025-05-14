b2048f9457131c822f9a68cb9c51ea1fe86c6ac1
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-05-13 18:37:43 -0600

    refactor: remove Flowbite dependencies and update TailwindCSS
    Removed Flowbite-related imports, plugins, and dependencies from the codebase to streamline the project and reduce unnecessary dependencies. Updated TailwindCSS to version 4.0.0 for improved compatibility and performance. Adjusted the Vite configuration and package.json accordingly.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite";
 import fs from 'fs'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss()],
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'ssl/key.pem')),
